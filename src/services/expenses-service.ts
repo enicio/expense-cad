84002ede62f76420ed8525e2ab676ed355b75378
enicio <jessandro42@gmail.com>
2024-05-08 23:48:20 -0300

    refactor: start refactor bull queue implementation

--- src/services/expenses-service.ts
+++ src/services/expenses-service.ts
@@ -6,7 +6,7 @@ import {
 } from '../repository/expense-repository'
 import { UserRepository } from '../repository/user-repository'
 
-import { Queue } from 'bullmq'
+import { Queue, Worker } from 'bullmq'
 
 const REDIS_HOST = '0.0.0.0'
 const REDIS_PORT = 6379
@@ -15,15 +15,40 @@ const QUEUE_NAME = 'foo'
 const newQueue = new Queue(QUEUE_NAME, {
   connection: {
     host: REDIS_HOST,
-    port: REDIS_PORT
+    port: REDIS_PORT,
   },
+})
+
+const worker = new Worker(
+  'foo',
+  async (job) => {
+    const { expense, user } = job.data
+    await sendEmail(expense, user)
+  },
+  {
+    connection: {
+      host: REDIS_HOST,
+      port: REDIS_PORT,
+    },
+  },
+)
+
+worker.on('completed', (job: any) => {
+  console.log(`${job.id} has completed!`)
+})
+
+worker.on('active', (job: any) => {
+  console.log(`${job.id} has started!`)
+})
+
+worker.on('failed', (job: any, err: any) => {
+  console.log(`${job?.id} has failed with ${err.message}`)
 })
 
 export class ExpenseService {
   constructor(
     private expenseRepository: ExpenseRepository,
-    private userRepository: UserRepository,
-    private myQueue = newQueue
+    private userRepository: UserRepository, // private myQueue = newQueue,
   ) {}
 
   async createExpense(data: expenseProps) {
@@ -35,11 +60,10 @@ export class ExpenseService {
     }
     try {
       // Enfileirando envio de email
-      queueEmail(expense, user, this.myQueue, sendEmail)
+      queueEmail(expense, user, newQueue)
     } catch (error: any) {
       console.error(error.message)
     }
-    // Simulando envio de email - observar resultado no console
     return { expense }
   }
 
