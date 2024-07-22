86a17286fc98d4f75b71d053e82d8cc444280d53
enicio <jessandro42@gmail.com>
2024-07-21 22:18:14 -0300

    feat: add nodemailer

--- src/lib/send-email.ts
+++ src/lib/send-email.ts
@@ -1,51 +1,28 @@
-import { randomInt } from 'crypto'
 import { expenseProps } from '../repository/expense-repository'
 import { createUserProps } from '../repository/user-repository'
+import nodemailer from 'nodemailer'
+import { getMailClient } from './mail'
+import { dayjs } from './dayjs'
 
 export const sendEmail = async (
   expense: expenseProps,
   user: createUserProps,
 ) => {
+  const transporter = await getMailClient()
   const emailData = {
-    from: 'noreply@seudominio.com.br',
+    from: 'jessandro@gmail.com',
     to: user.email,
     subject: 'Despesa cadastrada com sucesso!',
     text: `Olá ${user.name},
-
       Sua nova despesa foi cadastrada com sucesso!
-
       Descrição: ${expense.description}
-      Data: ${expense.date}
+      Data: ${dayjs(expense.date).format('DD/MM/YYYY')}
       Valor: R$ ${expense.amount}
-
       Atenciosamente,
-
       Equipe do Sistema de Despesas`,
   }
-  const sendMailPromisse = new Promise((resolve, reject) => {
-    let x = true
-    const random = randomInt(1, 10)
-    console.log(`Random: ${random}`)
-    random > 5 ? (x = true) : (x = false)
 
-    if (x) {
-      setTimeout(() => {
-        resolve('Email enviado com sucesso!')
-      }, 3000)
-    } else {
-      reject(new Error('Erro ao enviar email!'))
-    }
-  })
-  /* console.log('----------------------------------------------------')
-  console.log('Simulando envio de email:')
-  console.log('----------------------------------------------------')
-  console.log(`De: ${emailData.from}`)
-  console.log(`Para: ${emailData.to}`)
-  console.log(`Assunto: ${emailData.subject}`)
-  console.log(`Conteúdo: ${emailData.text}`)
-  console.log('----------------------------------------------------')
-*/
-  return sendMailPromisse.then((res) => {
-    console.log(res)
-  })
+  const info = await transporter.sendMail(emailData)
+  console.log(nodemailer.getTestMessageUrl(info))
+  return info
 }
